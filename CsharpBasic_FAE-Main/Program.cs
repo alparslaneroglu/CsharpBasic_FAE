0192833471e41594831db175bb06ccd3280cbebb
alparslaneroglu <furkanalparslaneroglu@gmail.com>
2021-11-01 09:12:18 +0300

    ilkdegisiklik

--- CsharpBasic_FAE-Main/Program.cs
+++ CsharpBasic_FAE-Main/Program.cs
@@ -17,7 +17,7 @@ namespace CsharpBasic_FAE_Main
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new AnaForm());
         }
     }
 }
